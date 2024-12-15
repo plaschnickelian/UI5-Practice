aab355a47bd3e2c0e6364d59776b9bdcca0d587f
Elian Plaschnick <elian.plaschnick@outlook.de>
2024-12-15 20:50:49 +0100

    added message toast to button

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], (Controller, MessageToast) => {
     "use strict";
 
     return Controller.extend("sap.ui.demo.practice.controller.App", {
         onShowHello() {
-            alert("Hello World");
+            MessageToast.show("Hello World");
         }
     })
 })
